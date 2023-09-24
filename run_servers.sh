db1d2abf281b4358c41da7d8bd3c88920b35af89
Tuomo <tuokri@tuta.io>
2023-09-24 23:39:39 +0300

    Server runner script

---
+++ run_servers.sh
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env bash
+
+set -e
+
+export OVERMIND_NO_PORT=1
+export OVERMIND_ANY_CAN_DIE=1
+export OVERMIND_AUTO_RESTART=socat1,socat2,steamds1,steamds2,a2sserver1,a2sserver2
+
+overmind start
