1e30af2c6ccc95bd7d63e70b1a240c67dd21a330
Tuomo <tuokri@tuta.io>
2023-09-24 23:39:26 +0300

    Add socat_wrapper.py

---
+++ socat_wrapper.py
@@ -0,0 +1,33 @@
+﻿import argparse
+import subprocess
+import tomllib
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("file")
+    args = ap.parse_args()
+    file = args.file
+
+    with open(file, "rb") as f:
+        cfg = tomllib.load(f)
+    
+    # TODO: proper config.
+    gameport = cfg["GAMEPORT"]
+    dest_host = ""
+    dest_gameport = ""
+
+    subprocess.check_call([
+        "socat",
+        "-d",
+        "-d",
+        "-d",
+        "-t1",
+        "-T5",
+        f"UDP-LISTEN{gameport},fork,reuseaddr",
+        f"UDP:{dest_host}:{dest_gameport}",
+    ])
+
+
+if __name__ == "__main__":
+    main()
