01af5d408b3c4ae657e10697fb9b6cad26addadf
Tuomo <tuokri@tuta.io>
2024-02-16 18:29:21 +0200

    Set OVERMIND_AUTO_RESTART=all

--- run_servers.sh
+++ run_servers.sh
@@ -4,6 +4,6 @@ set -e
 
 export OVERMIND_NO_PORT=1
 export OVERMIND_ANY_CAN_DIE=1
-export OVERMIND_AUTO_RESTART=socat1,socat2,steamds1,steamds2,a2sserver1,a2sserver2
+export OVERMIND_AUTO_RESTART=all
 
 overmind start
